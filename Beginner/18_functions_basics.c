32cfa990370972e90a5cf60f2b2c5594f9ecc43d
Sercan <sercanozver@gmail.com>
2025-12-27 15:40:04 +0300

    Update 18_functions_basics.c

--- Beginner/18_functions_basics.c
+++ Beginner/18_functions_basics.c
@@ -90,6 +90,150 @@ int main(void)
 
 /******************************************************************************/
 
+#include <stdio.h>
+
+/*
+    Program Name: Exponential Calculation
+    Description:
+    This program calculates e power x
+    using series expansion and functions.
+*/
+
+/*
+    Function Name: factorial
+    Description:
+    This function calculates factorial of a number.
+*/
+float factorial(int k)
+{
+    int i;
+    float f = 1;
+
+    /* Multiply numbers from 2 to k */
+    for (i = 2; i <= k; i++)
+    {
+        f = f * i;
+    }
+
+    return f;
+}
+
+/*
+    Function Name: power
+    Description:
+    This function calculates x power n.
+*/
+float power(int x, int n)
+{
+    int i;
+    float f = 1;
+
+    /* Multiply x, n times */
+    for (i = 1; i <= n; i++)
+    {
+        f = f * x;
+    }
+
+    return f;
+}
+
+int main(void)
+{
+    int i, n, x;
+    float e;
+
+    /* Get n value */
+    printf("n value: ");
+    scanf("%d", &n);
+
+    /* Get x value */
+    printf("x value: ");
+    scanf("%d", &x);
+
+    e = 1;
+
+    /* Calculate e power x using series */
+    for (i = 1; i <= n; i++)
+    {
+        e = e + power(x, i) / factorial(i);
+    }
+
+    /* Print result */
+    printf("e power %d = %f", x, e);
+
+    return 0;
+}
+
 
 
 /******************************************************************************/
+
+#include <stdio.h>
+
+/*
+    Program Name: Cosine Calculation
+    Description:
+    This program calculates cosine(x)
+    using functions, loop and series.
+*/
+
+/*
+    Function Name: power
+    Description:
+    This function calculates x to the power n.
+*/
+float power(int x, int n)
+{
+    int i;
+    float result = 1;
+
+    /* Multiply x, n times */
+    for (i = 1; i <= n; i++)
+    {
+        result = result * x;
+    }
+
+    return result;
+}
+
+/*
+    Function Name: factorial
+    Description:
+    This function calculates factorial of n.
+*/
+float factorial(int n)
+{
+    int i;
+    float result = 1;
+
+    /* Multiply numbers from 1 to n */
+    for (i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+
+    return result;
+}
+
+int main(void)
+{
+    int i, n, x, sign;
+    float cosine;
+
+    /* Get N value */
+    printf("Enter N value: ");
+    scanf("%d", &n);
+
+    /* Get X value */
+    printf("Enter X value: ");
+    scanf("%d", &x);
+
+    cosine = 1.0;
+    sign = -1;
+    i = 2;
+
+    /* Calculate cosine series */
+    while (i <= 2 * n)
+    {
+        cosine = cosine + sign * power(x, i) / factorial(i);
+        sign
